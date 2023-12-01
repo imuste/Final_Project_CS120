2d5cc24554b36638498b699838e2a10bdf8422ce
ModernGonzo06 <102342069+ModernGonzo06@users.noreply.github.com>
2023-12-01 17:35:14 +0000

    updated html and js

--- recipe.js
+++ recipe.js
@@ -1,16 +1,11 @@
-// Define an array to store the saved recipes
 let savedRecipes = [];
-//var urlString = "https://www.themealdb.com/api/json/v1/1/filter.php?a="
-// URL to fetch the list of Mexican meals for testing
-var url = "https://www.themealdb.com/api/json/v1/1/filter.php?a=Mexican";
+let currentRecipeIndex = 0;
 
 var url = localStorage.getItem("country_url");
 url = url.replace(/^"(.*)"$/, "$1"); // Removes quotes from the beginning and end of the string
 
-// URL to fetch details of a specific meal by ID
 var id_url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
 
-// Fetches a list of Mexican meals
 async function use_mealDB_country(){
     try {
         const response = await fetch(url);
@@ -21,7 +16,6 @@ async function use_mealDB_country(){
     }
 }
 
-// Processes the fetched list of meals
 async function process_country(data){
     if (data && data.meals) {
         const meals = data.meals;
@@ -39,26 +33,23 @@ async function process_country(data){
     }
 }
 
-// Fetches details of selected recipes by their IDs
 async function full_meal_lookups(selectedRecipes){
     for (let recipe of selectedRecipes){
         await use_mealDB_id(recipe.id);
     }
 }
 
-// Fetches details of a specific recipe by ID
 async function use_mealDB_id(id){
     try {
         const this_url = id_url + id;
         const response = await fetch(this_url);
         const data = await response.json();
-        await process_id(data);
+        process_id(data);
     } catch (error) {
         console.log(error);
     }
 }
 
-// Processes details of a specific recipe
 function process_id(data){
     const dishName = data.meals[0].strMeal;
     const youtubeURL = data.meals[0].strYoutube;
@@ -67,15 +58,14 @@ function process_id(data){
     savedRecipes.push(newRecipe);
 }
 
-// Displays the saved recipes with details
 async function displaySavedRecipes() {
-    savedRecipes = []; // Reset the array before fetching new recipes
-    await use_mealDB_country();
-
+    
     const recipesContainer = document.getElementById('recipes-container');
     recipesContainer.innerHTML = ''; // Clear the container before displaying updated content
 
-    savedRecipes.forEach(recipe => {
+    if (savedRecipes.length > 0) {
+        const recipe = savedRecipes[currentRecipeIndex]; // Use the currentRecipeIndex
+
         const recipeItem = document.createElement('div');
         recipeItem.classList.add('recipe-item');
 
@@ -102,8 +92,34 @@ async function displaySavedRecipes() {
         recipeItem.appendChild(viewButton);
 
         recipesContainer.appendChild(recipeItem);
-    });
+    }
 }
 
-// Call the function to display saved recipes when the page loads
-window.onload = displaySavedRecipes;
+window.onload = async function() {
+    savedRecipes = []; // Reset the array before fetching new recipes
+    await use_mealDB_country();
+    displaySavedRecipes();
+
+    const prevButton = document.getElementById('prevButton');
+    const nextButton = document.getElementById('nextButton');
+
+    // "Next" button functionality
+    nextButton.addEventListener('click', () => {
+        if (currentRecipeIndex < savedRecipes.length - 1) {
+            currentRecipeIndex++;
+        } else {
+            currentRecipeIndex = 0; // Loop back to the first recipe
+        }
+        displaySavedRecipes(); // Display the next recipe
+    });
+
+    // "Previous" button functionality
+    prevButton.addEventListener('click', () => {
+        if (currentRecipeIndex > 0) {
+            currentRecipeIndex--;
+        } else {
+            currentRecipeIndex = savedRecipes.length - 1; // Go to the last recipe
+        }
+        displaySavedRecipes(); // Display the previous recipe
+    });
+};
