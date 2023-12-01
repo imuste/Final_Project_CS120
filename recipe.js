254af0b365a99a3a19c5e4c7badf8bda8f7e759e
ModernGonzo06 <102342069+ModernGonzo06@users.noreply.github.com>
2023-12-01 02:31:52 -0500

    Update recipe.js
    added replace method to remove "" that were included on url string

--- recipe.js
+++ recipe.js
@@ -1,9 +1,12 @@
 // Define an array to store the saved recipes
 let savedRecipes = [];
-
+var urlString = "https://www.themealdb.com/api/json/v1/1/filter.php?a="
 // URL to fetch the list of Mexican meals for testing
-var url = "https://www.themealdb.com/api/json/v1/1/filter.php?a=Mexican";
-//var url = localStorage.getItem('country_url')
+//var url = "https://www.themealdb.com/api/json/v1/1/filter.php?a=Mexican";
+
+var url = localStorage.getItem("country_url");
+url = url.replace(/^"(.*)"$/, "$1"); // Removes quotes from the beginning and end of the string
+
 // URL to fetch details of a specific meal by ID
 var id_url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=";
 
